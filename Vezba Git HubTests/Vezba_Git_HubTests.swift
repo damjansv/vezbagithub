4150786731ad6da0b135374cd2fcd726d339809e
damjansv <30604197+damjansv@users.noreply.github.com>
2017-08-01 20:43:54 +0200

    Initial Commit

---
+++ Vezba Git HubTests/Vezba_Git_HubTests.swift
@@ -0,0 +1,36 @@
+//
+//  Vezba_Git_HubTests.swift
+//  Vezba Git HubTests
+//
+//  Created by AD on 8/1/17.
+//  Copyright © 2017 AD. All rights reserved.
+//
+
+import XCTest
+@testable import Vezba_Git_Hub
+
+class Vezba_Git_HubTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
